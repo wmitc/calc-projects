e00ff1517557eb0efba410b01f7b49e352f26fe5
William Mitchell <william.b.mitchell46.mil@army.mil>
2025-05-01 15:02:38 -0400

    Fixed input validation in SimpleCalc; using strtol with error checking; added Makefile

--- 1_SimpleCalc/src/math_utils.c
+++ 1_SimpleCalc/src/math_utils.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "math_utils.h"
 
 #define MAX_BITS 32
@@ -10,8 +11,8 @@
 // Performs addition operation
 int32_t add(int32_t a, int32_t b){
     if(a + b > INT32_MAX){
-        fprintf(stderr, "[-] Integer overflow detected!\n");
-        return -1;
+        fprintf(stderr, "[-] Integer overflow detected in addition!\n");
+        exit(EXIT_FAILURE);
     }
 	return a + b;
 }
@@ -19,8 +20,8 @@ int32_t add(int32_t a, int32_t b){
 // Performs subraction operation
 int32_t subtract(int32_t a, int32_t b){
     if(a - b < INT32_MIN){
-        fprintf(stderr, "[-] Integer overflow detected!\n");
-        return -1;
+        fprintf(stderr, "[-] Integer overflow detected in subtraction!\n");
+        exit(EXIT_FAILURE);
     }
 	return a - b;
 }
@@ -29,8 +30,8 @@ int32_t subtract(int32_t a, int32_t b){
 int32_t multiply(int32_t a, int32_t b){
     int64_t temp = (int64_t) a * (int64_t) b;
     if(temp > INT32_MAX || temp < INT32_MIN){
-        fprintf(stderr, "[-] Integer overflow detected!\n");
-        return -1;
+        fprintf(stderr, "[-] Integer overflow detected in multiplcation!\n");
+        exit(EXIT_FAILURE);
     } 
 	return a * b;
 }
@@ -38,8 +39,8 @@ int32_t multiply(int32_t a, int32_t b){
 // Performs divide operation
 int32_t divide(int32_t a, int32_t b){
     if(b == 0){
-        fprintf(stderr, "[-] Divide by zero error!");
-        return -1;
+        fprintf(stderr, "[-] Divide by zero error in divide!\n");
+        exit(EXIT_FAILURE);
     }
     return a / b;
 }
@@ -47,8 +48,8 @@ int32_t divide(int32_t a, int32_t b){
 // Performs modulo operation
 int32_t modulo(int32_t a, int32_t b){
     if(b == 0){
-        fprintf(stderr, "[-] Divide by zero error!");
-        return -1;
+        fprintf(stderr, "[-] Divide by zero error in modulo!\n");
+        exit(EXIT_FAILURE);
     }
     return a % b;
 }
@@ -57,8 +58,8 @@ int32_t modulo(int32_t a, int32_t b){
 int32_t shift_left(int32_t a, int32_t b){
     printf("%d\n", a);
     if(b < 0 || b >= 32){
-        fprintf(stderr, "[-] Bad input!");
-        return -1;       
+        fprintf(stderr, "[-] Bad input for shift left!\n");
+        exit(EXIT_FAILURE);       
     }
     return a << b;
 }
@@ -66,8 +67,8 @@ int32_t shift_left(int32_t a, int32_t b){
 // Performs right shift operation
 int32_t shift_right(int32_t a, int32_t b){
     if(b < 0 || b >= 32){
-        fprintf(stderr, "[-] Bad input!");
-        return -1;       
+        fprintf(stderr, "[-] Bad input for shift right!\n");
+        exit(EXIT_FAILURE);       
     }
     return a >> b;
 }
@@ -75,8 +76,8 @@ int32_t shift_right(int32_t a, int32_t b){
 // Performs bitwise AND operation
 int32_t bitwise_and(int32_t a, int32_t b){
     if(a < 0 || b < 0){
-        fprintf(stderr, "[-] Bad input!");
-        return -1;       
+        fprintf(stderr, "[-] Bad input for AND!\n");
+        exit(EXIT_FAILURE);       
     }
     return a & b;
 }
@@ -84,8 +85,8 @@ int32_t bitwise_and(int32_t a, int32_t b){
 // Performs bitwise OR operation
 int32_t bitwise_or(int32_t a, int32_t b){
     if(a < 0 || b < 0){
-        fprintf(stderr, "[-] Bad input!");
-        return -1;       
+        fprintf(stderr, "[-] Bad input for OR!\n");
+        exit(EXIT_FAILURE);       
     }
     return a | b;
 }
@@ -93,8 +94,8 @@ int32_t bitwise_or(int32_t a, int32_t b){
 // Performs bitwise XOR operation
 int32_t bitwise_xor(int32_t a, int32_t b){
     if(a < 0 || b < 0){
-        fprintf(stderr, "[-] Bad input!");
-        return -1;       
+        fprintf(stderr, "[-] Bad input for XOR!\n");
+        exit(EXIT_FAILURE);       
     }
     return a ^ b;
 }
@@ -102,8 +103,8 @@ int32_t bitwise_xor(int32_t a, int32_t b){
 // Performs bitwise left rotation operation
 int32_t rotate_left(int32_t a, int32_t b){
     if(a < 0 || b < 0){
-        fprintf(stderr, "[-] Bad input!");
-        return -1;       
+        fprintf(stderr, "[-] Bad input for rotate left!\n");
+        exit(EXIT_FAILURE);       
     } 
     // Ensure b is in range
     b %= MAX_BITS;
@@ -114,8 +115,8 @@ int32_t rotate_left(int32_t a, int32_t b){
 // Performs bitwise right rotation operation
 int32_t rotate_right(int32_t a, int32_t b){
     if(a < 0 || b < 0){
-        fprintf(stderr, "[-] Bad input!");
-        return -1;       
+        fprintf(stderr, "[-] Bad input for rotate right!\n");
+        exit(EXIT_FAILURE);       
     }
     // Ensure b is in range
     b %= MAX_BITS;
--- 1_SimpleCalc/src/simplecalc.c
+++ 1_SimpleCalc/src/simplecalc.c
@@ -4,29 +4,68 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include "math_utils.h"
+
+#define BASE10 10
 
 int main(int argc, char *argv[]){
     if(argc <= 1){
         printf("No arguments provided\n");
-        
         return 1;
     }
 
     if(argc != 4){
         printf("Usage: %s <number 1> <operation> <number 2>\n", argv[0]);
-    
         return 1;
     }
     
     // Parse the command-line arguments
-    int32_t num1 = atoi(argv[1]);
     char *op = argv[2];
-    int32_t num2 = atoi(argv[3]);  
-    //printf("Hello world!\n");        
-    //printf("%d",num1); printf("\n");
-    //printf("%s",op); printf("\n");
-    //printf("%d",num2); printf("\n");
+
+    char *endptr;
+    errno = 0; // Set errno to 0 before strtol call
+    long long_num1 = strtol(argv[1], &endptr, BASE10);
+
+    // Check for errors
+    if(errno == ERANGE)
+    {
+        fprintf(stderr, "[-] strtol range error!\n");
+        return 1;
+    }
+    if(*endptr != '\0')
+    {
+        fprintf(stderr, "[-] bad input error!\n");
+        return 1;
+    }
+    if(long_num1 < INT32_MIN || long_num1 > INT32_MAX)
+    {
+        fprintf(stderr, "[-] integer overflow error!\n");
+        return 1;
+    }   
+    errno = 0; // Set errno to 0 before strtol call
+    long long_num2 = strtol(argv[3], &endptr, BASE10);  
+
+    // Check for errors
+    if(errno == ERANGE)
+    {
+        fprintf(stderr, "[-] strtol range error!\n");
+        return 1;
+    }
+    if(*endptr != '\0')
+    {
+        fprintf(stderr, "[-] malformed input error!\n");
+        return 1;
+    }
+    if(long_num2 < INT32_MIN || long_num2 > INT32_MAX)
+    {
+        fprintf(stderr, "[-] integer overflow error!\n");
+        return 1;
+    }  
+    
+    // Cast long_num1 and long_num2 as int32_t
+    int32_t num1 = (int32_t) long_num1;
+    int32_t num2 = (int32_t) long_num2;
 
     // Variable to hold result
     int32_t result = 0;
